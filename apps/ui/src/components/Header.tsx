27a45db73d2612080a4508334d7f34387478ad0a
JamesTLopez <JamesTLopez.code@gmail.com>
2024-11-12 14:38:17 -0500

    feat(daco-21): add icon to header

--- apps/ui/src/components/Header.tsx
+++ apps/ui/src/components/Header.tsx
@@ -1,7 +1,18 @@
-import { Button, ConfigProvider, Flex, Layout, Typography } from 'antd';
+import { Button, ConfigProvider, Flex, Image, Layout, Typography } from 'antd';
 import { pcglHeaderTheme } from './providers/ThemeProvider';
 const { Link } = Typography;
 const { Header } = Layout;
+
+const imageStyle: React.CSSProperties = {
+	width: '100%',
+	maxWidth: 200,
+	minWidth: 170,
+};
+
+const linkStyle: React.CSSProperties = {
+	minWidth: 100,
+	textAlign: 'center',
+};
 
 const HeaderComponent = () => {
 	return (
@@ -9,14 +20,25 @@ const HeaderComponent = () => {
 			<Header>
 				<Flex style={{ height: '100%' }} justify="center" align="center">
 					<Flex flex={1}>
-						<Flex justify="space-around" align="center" gap={30}>
-							<Link target="_blank">Policies & Guidelines</Link>
-							<Link target="_blank">Help Guides</Link>
-							<Link target="_blank">Controlled Data Users</Link>
+						<Flex justify="space-around" align="center" gap={40}>
+							<Link target="_blank">
+								<Image style={imageStyle} src="../../public/pcgl-logo-full.png" preview={false} />
+							</Link>
+							<Link style={linkStyle} target="_blank">
+								Policies & Guidelines
+							</Link>
+							<Link style={linkStyle} target="_blank">
+								Help Guides
+							</Link>
+							<Link style={linkStyle} target="_blank">
+								Controlled Data Users
+							</Link>
 						</Flex>
 					</Flex>
 					<Flex justify="flex-end" align="center" gap={20}>
-						<Link target="_blank">Apply For Access</Link>
+						<Link style={linkStyle} target="_blank">
+							Apply For Access
+						</Link>
 						<Button variant="solid" color="primary">
 							Login
 						</Button>
