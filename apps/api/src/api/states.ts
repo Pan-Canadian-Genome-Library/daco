1b77170a39ad1ac343afa2d78fcfa67adbfe8b41
Dan DeMaria <ddemaria@oicr.on.ca>
2024-12-05 11:44:31 -0500

    Major Refactor

--- apps/api/src/api/states.ts
+++ apps/api/src/api/states.ts
@@ -18,7 +18,7 @@
  */
 
 import { ApplicationStates, ApplicationStateValues } from '@pcgl-daco/data-model/src/types.js';
-import { StateMachine, t as transition } from 'typescript-fsm';
+import { ITransition, StateMachine, t as transition } from 'typescript-fsm';
 import { getDbInstance } from '../db/index.js';
 import { applications } from '../db/schemas/applications.js';
 import applicationService from '../service/application-service.js';
@@ -58,70 +58,76 @@ const validateContent = async (
 // TODO: Replace with individual methods
 type ApplicationTransitionCallback = () => AsyncResult<typeof applications.$inferSelect>;
 
-type TransitionValues = [
+type ApplicationTransitions = ITransition<
 	ApplicationStateValues,
 	ApplicationStateEvents,
-	ApplicationStateValues,
-	ApplicationTransitionCallback?,
-];
-
-// Draft
-const draftSubmitTransitionValues: TransitionValues = [DRAFT, submit, INSTITUTIONAL_REP_REVIEW];
-const draftEditTransitionValues: TransitionValues = [DRAFT, edit, DRAFT];
-const draftCloseTransitionValues: TransitionValues = [DRAFT, close, CLOSED];
-
-// Rep Review
-const repReviewCloseTransitionValues: TransitionValues = [INSTITUTIONAL_REP_REVIEW, close, CLOSED];
-const repReviewEditTransitionValues: TransitionValues = [INSTITUTIONAL_REP_REVIEW, edit, DRAFT];
-const repReviewRevisionTransitionValues: TransitionValues = [INSTITUTIONAL_REP_REVIEW, revision_request, REP_REVISION];
-const repReviewSubmitTransitionValues: TransitionValues = [INSTITUTIONAL_REP_REVIEW, submit, DAC_REVIEW];
-
-// Rep Revision
-const repRevisionSubmitTransitionValues: TransitionValues = [REP_REVISION, submit, INSTITUTIONAL_REP_REVIEW];
-
-// DAC Review
-const dacReviewApproveTransitionValues: TransitionValues = [DAC_REVIEW, approve, APPROVED];
-const dacReviewCloseTransitionValues: TransitionValues = [DAC_REVIEW, close, CLOSED];
-const dacReviewEditTransitionValues: TransitionValues = [DAC_REVIEW, edit, DRAFT];
-const dacReviewRevisionTransitionValues: TransitionValues = [DAC_REVIEW, revision_request, DAC_REVISIONS_REQUESTED];
-const dacReviewRejectTransitionValues: TransitionValues = [DAC_REVIEW, reject, REJECTED];
-
-// DAC Revision
-const dacRevisionSubmitTransitionValues: TransitionValues = [DAC_REVISIONS_REQUESTED, submit, DAC_REVIEW];
-
-// Approval
-const approvalRevokedTransitionValues: TransitionValues = [APPROVED, revoked, REVOKED];
-
-// All Transitions
-const applicationTransitionValues: TransitionValues[] = [
-	draftSubmitTransitionValues,
-	draftEditTransitionValues,
-	draftCloseTransitionValues,
-	repReviewCloseTransitionValues,
-	repReviewEditTransitionValues,
-	repReviewRevisionTransitionValues,
-	repReviewSubmitTransitionValues,
-	repRevisionSubmitTransitionValues,
-	dacReviewApproveTransitionValues,
-	dacReviewCloseTransitionValues,
-	dacReviewEditTransitionValues,
-	dacReviewRevisionTransitionValues,
-	dacReviewRejectTransitionValues,
-	dacRevisionSubmitTransitionValues,
-	approvalRevokedTransitionValues,
-];
-
-const getTransitionHandler = (values: TransitionValues) => {
-	return transition(...values);
-};
+	ApplicationTransitionCallback
+>;
 
 export class ApplicationStateManager extends StateMachine<ApplicationStateValues, ApplicationStateEvents> {
 	private readonly _id: number;
 	private readonly _application: typeof applications.$inferSelect;
 	private readonly _initState: ApplicationStateValues;
 
+	// Draft
+	private draftSubmitTransition = transition(DRAFT, submit, INSTITUTIONAL_REP_REVIEW, this._onSubmit);
+	private draftEditTransition = transition(DRAFT, edit, DRAFT, this._onSubmit);
+	private draftCloseTransition = transition(DRAFT, close, CLOSED, this._onSubmit);
+
+	// Rep Review
+	private repReviewCloseTransition = transition(INSTITUTIONAL_REP_REVIEW, close, CLOSED, this._onSubmit);
+	private repReviewEditTransition = transition(INSTITUTIONAL_REP_REVIEW, edit, DRAFT, this._onSubmit);
+	private repReviewRevisionTransition = transition(
+		INSTITUTIONAL_REP_REVIEW,
+		revision_request,
+		REP_REVISION,
+		this._onSubmit,
+	);
+	private repReviewSubmitTransition = transition(INSTITUTIONAL_REP_REVIEW, submit, DAC_REVIEW, this._onSubmit);
+
+	// Rep Revision
+	private repRevisionSubmitTransition = transition(REP_REVISION, submit, INSTITUTIONAL_REP_REVIEW, this._onSubmit);
+
+	// DAC Review
+	private dacReviewApproveTransition = transition(DAC_REVIEW, approve, APPROVED, this._onSubmit);
+	private dacReviewCloseTransition = transition(DAC_REVIEW, close, CLOSED, this._onSubmit);
+	private dacReviewEditTransition = transition(DAC_REVIEW, edit, DRAFT, this._onSubmit);
+	private dacReviewRevisionTransition = transition(
+		DAC_REVIEW,
+		revision_request,
+		DAC_REVISIONS_REQUESTED,
+		this._onSubmit,
+	);
+	private dacReviewRejectTransition = transition(DAC_REVIEW, reject, REJECTED, this._onSubmit);
+
+	// DAC Revision
+	private dacRevisionSubmitTransition = transition(DAC_REVISIONS_REQUESTED, submit, DAC_REVIEW, this._onSubmit);
+
+	// Approval
+	private approvalRevokedTransition = transition(APPROVED, revoked, REVOKED, this._onSubmit);
+
+	// All Transitions
+	private applicationTransitions: ApplicationTransitions[] = [
+		this.draftSubmitTransition,
+		this.draftEditTransition,
+		this.draftCloseTransition,
+		this.repReviewCloseTransition,
+		this.repReviewEditTransition,
+		this.repReviewRevisionTransition,
+		this.repReviewSubmitTransition,
+		this.repRevisionSubmitTransition,
+		this.dacReviewApproveTransition,
+		this.dacReviewCloseTransition,
+		this.dacReviewEditTransition,
+		this.dacReviewRevisionTransition,
+		this.dacReviewRejectTransition,
+		this.dacRevisionSubmitTransition,
+		this.approvalRevokedTransition,
+	];
+
 	// TODO: Add methods for all actions: submit, close, edit, revision_request, approve, reject, revoked
 	private async _onSubmit() {
+		// throw new Error('i messed up');
 		if (this.can(submit)) {
 			// TODO: Add Validation
 			const validationResult = await validateContent(this._application);
@@ -142,12 +148,7 @@ export class ApplicationStateManager extends StateMachine<ApplicationStateValues
 		this._initState = state;
 		this._application = application;
 
-		const applicationTransitions = applicationTransitionValues.map((values) => {
-			values.push(this._onSubmit);
-			return getTransitionHandler(values);
-		});
-
-		this.addTransitions(applicationTransitions);
+		this.addTransitions(this.applicationTransitions);
 	}
 
 	get id() {
@@ -156,6 +157,15 @@ export class ApplicationStateManager extends StateMachine<ApplicationStateValues
 
 	get state() {
 		return this.getState();
+	}
+
+	submitDraft(props: {}) {
+		//if can submit
+		// try {
+		// this.dispatch(ApplicationStateEvents.submit);
+		// } catch (e) {
+		// }
+		// else return error
 	}
 }
 
@@ -173,3 +183,5 @@ export const createApplicationStateManager = async ({ id }: { id: number }) => {
 
 	return success(appStateManager);
 };
+
+// application.submitDraft({});
