a1fda155b62cd10ee2b86ba6c672c0be46a98f05
JamesTLopez <JamesTLopez.code@gmail.com>
2025-01-08 10:25:40 -0500

    testing third-party bindings with rfh antd and zod

--- apps/ui/src/pages/applications/sections/applicant.tsx
+++ apps/ui/src/pages/applications/sections/applicant.tsx
@@ -18,84 +18,132 @@
  */
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form as AntdForm, Flex, Input } from 'antd';
-import Title from 'antd/es/typography/Title';
-import { useForm } from 'react-hook-form';
+import { Divider, Flex, Form, Input, Radio, Select, Typography } from 'antd';
+import { createSchemaFieldRule } from 'antd-zod';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { FormItem } from 'react-hook-form-antd';
+
 import * as z from 'zod';
 
 import ApplicationWrapper from '@/components/layouts/SectionWrapper';
 import SectionFooter from '@/components/pages/application/SectionFooter';
 
+const { Item } = Form;
+const { Title } = Typography;
+
 type FieldType = {
-	firstName?: string;
-	middleName?: string;
-	lastName?: string;
-	suffix?: string;
+	firstName: string;
+	suffix: string;
+	randomNum: string | number;
 };
 
 const schema = z.object({
 	firstName: z
 		.string()
 		.min(1, { message: 'Required' })
-		.max(15, { message: 'Username should be less than 15 characters' }),
-	middleName: z.string().max(15, { message: 'Username should be less than 15 characters' }),
-	lastName: z
-		.string()
-		.min(1, { message: 'Required' })
-		.max(15, { message: 'Username should be less than 15 characters' }),
+		.max(15, { message: 'First name should be less than 15 characters' }),
 	suffix: z.string().min(1, { message: 'Required' }),
+	randomNum: z.string(),
 });
 
+const rules = createSchemaFieldRule(schema);
+
+/**
+ * 	Main libs: react-hook-form, Zod, Antd
+ *  recommended-libraries: react-hook-form-antd, antd-zod
+ */
+
 const Applicant = () => {
-	const { handleSubmit, register } = useForm({
-		defaultValues: { firstName: '', middleName: '', lastName: '', suffix: '' },
+	const {
+		handleSubmit,
+		formState: { errors },
+		control,
+	} = useForm<FieldType>({
 		resolver: zodResolver(schema),
 	});
+
+	const onSubmit: SubmitHandler<FieldType> = (data) => {
+		console.log(data);
+	};
+
+	const onSubmitAntd = (values: unknown) => {
+		console.log('antd-no-rhf: ', values);
+	};
+
+	console.log(errors);
 
 	return (
 		<ApplicationWrapper>
 			<>
-				<Flex vertical>
-					<Title level={2}>Applicant Information (Principal Investigator)</Title>
+				{/* just react-hook-form / antd / zod  */}
+				{/* If we were not to use antd or react-hook-form antd */}
+				{/*Needs to utilize the <Controller/> component to interact with antd could possibly make a custom component*/}
+				<Flex style={{ width: '100%' }} vertical>
+					<Title level={4}>With RHF + antd-zod (RHF/antd/zod/antd-zod)</Title>
+					<Form style={{ width: '100%' }} onFinish={handleSubmit(onSubmit)} layout="vertical">
+						<Controller
+							name="firstName"
+							control={control}
+							render={({ field: { onChange, value } }) => {
+								return (
+									<Item label="First Name" name={'firstName'} rules={[rules]}>
+										<Input onChange={onChange} value={value} />
+									</Item>
+								);
+							}}
+						/>
+
+						<input type="submit" />
+					</Form>
 				</Flex>
-				<Flex style={{ width: '100%' }}>
-					<AntdForm
-						onFinish={handleSubmit((data) => {
-							console.log(data);
-						})}
-						style={{ width: '100%' }}
-					>
-						<Flex style={{ width: '100%' }}>
-							<AntdForm.Item
-								label="First Name"
-								style={{ width: '100%' }}
-								{...register('firstName', { required: true })}
-							>
-								<Input />
-							</AntdForm.Item>
 
-							<AntdForm.Item
-								label="Middle Name"
-								style={{ width: '100%' }}
-								{...register('middleName', { required: true })}
-							>
-								<Input />
-							</AntdForm.Item>
-						</Flex>
-						<Flex style={{ width: '100%' }}>
-							<AntdForm.Item<FieldType>
-								label="Last Name"
-								{...register('lastName', { required: true })}
-								name="lastName"
-								style={{ width: '100%' }}
-								rules={[{ required: true, message: 'Please input your username!' }]}
-							>
-								<Input />
-							</AntdForm.Item>
+				<Divider style={{ borderColor: '#7cb305' }} />
 
-							<input type="submit" />
-						</Flex>
-					</AntdForm>
+				{/* antd / zod / antd-zod */}
+				{/* antd-zod is supported and recommended on zod's official website */}
+				<Flex vertical>
+					<Title level={4}>No RHF (antd / zod / antd-zod)</Title>
+
+					<Form style={{ width: '100%' }} onFinish={onSubmitAntd} layout="vertical">
+						<Item label="First Name" name={'firstName'} rules={[rules]}>
+							<Input />
+						</Item>
+
+						<input type="submit" />
+					</Form>
+				</Flex>
+
+				{/* antd / zod / react-hook-form / react-hook-form-antd*/}
+				{/* react-hook-form-antd is refered on react-hook-forms official website */}
+				{/* Using different components from antd */}
+				<Divider style={{ borderColor: '#7cb305' }} />
+				<Flex vertical>
+					<Title level={4}>With RHF + RHFA (RHF/antd/zod/react-hook-form-antd)</Title>
+
+					<Form style={{ width: '100%' }} onFinish={handleSubmit(onSubmit)} layout="vertical">
+						<FormItem control={control} name="firstName" label="firstName">
+							<Input />
+						</FormItem>
+
+						<FormItem control={control} name="suffix" label="Suffix">
+							<Select>
+								<Select.Option value="mr">Mr</Select.Option>
+								<Select.Option value="mrs">Mrs</Select.Option>
+								<Select.Option value="dr">Dr</Select.Option>
+							</Select>
+						</FormItem>
+
+						<FormItem control={control} name="randomNum" label="Number">
+							<Radio.Group>
+								<Radio value={'A'}>A</Radio>
+								<Radio value={'B'}>B</Radio>
+								<Radio value={'C'}>C</Radio>
+								<Radio value={4}>D</Radio>
+							</Radio.Group>
+						</FormItem>
+
+						<input type="submit" />
+					</Form>
 				</Flex>
 				<SectionFooter />
 			</>
