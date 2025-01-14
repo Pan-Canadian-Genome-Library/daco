e8b9c9a3bb5556a849ff4b27711e26a5bc5ad3fe
JamesTLopez <JamesTLopez.code@gmail.com>
2025-01-14 14:40:49 -0500

    chore(comment): more copyright date updates

--- apps/ui/src/components/providers/ThemeProvider.tsx
+++ apps/ui/src/components/providers/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 /*
- * Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
  *
  * This program and the accompanying materials are made available under the terms of
  * the GNU Affero General Public License v3.0. You should have received a copy of the
