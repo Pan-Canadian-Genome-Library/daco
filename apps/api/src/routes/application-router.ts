57dc329275b3b2ca8634fe518c1d369a30810ef4
suraj <4347494+surajgoraya@users.noreply.github.com>
2025-01-20 13:29:36 -0500

    #154: Create API Route to Get State Totals (#156)
    * wip: 🚧 work in progress for state
    
    * chore: update lint
    
    * feat: add metadata service, and tests
    
    * chore: update copyrights
    
    * chore: uncomment rep_revison
    
    This will currently throw an error, however, will be fixed in #89 / #133
    
    * fix failing test for unrelated service
    
    * docs: 📝 update swagger file
    
    * fix: use logger instead of console.log for errors
    
    * chore: lint
    
    * chore: simplify code
    
    * fix: adapt to changes introduced into main

--- apps/api/src/routes/application-router.ts
+++ apps/api/src/routes/application-router.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
  *
  * This program and the accompanying materials are made available under the terms of
  * the GNU Affero General Public License v3.0. You should have received a copy of the
@@ -20,7 +20,13 @@
 import bodyParser from 'body-parser';
 import express, { Request } from 'express';
 
-import { createApplication, editApplication, getAllApplications, getApplicationById } from '@/api/application-api.js';
+import {
+	createApplication,
+	editApplication,
+	getAllApplications,
+	getApplicationById,
+	getApplicationStateTotals,
+} from '@/api/application-api.js';
 
 const applicationRouter = express.Router();
 const jsonParser = bodyParser.json();
@@ -134,5 +140,32 @@ applicationRouter.get(
 		}
 	},
 );
+
+/**
+ * Gets the total of how many applications are in each state type (APPROVED, REJECTED, etc...),
+ * including a TOTAL count.
+ *
+ * TODO:
+ * 	- Currently no validation is done to ensure that the current logged in user can access the specified application. This should be done and refactored.
+ * 	- Validate request params using Zod.
+ */
+applicationRouter.get('/applications/metadata/counts', async (req: Request<{}, {}, {}, any>, res) => {
+	const { userId } = req.query;
+
+	if (!userId) {
+		res.status(400).send({ message: 'User Id is Required' });
+		return;
+	}
+
+	const result = await getApplicationStateTotals({
+		userId,
+	});
+
+	if (result.success) {
+		res.status(200).send(result.data);
+	} else {
+		res.status(500).send({ message: result.message, errors: String(result.errors) });
+	}
+});
 
 export default applicationRouter;
