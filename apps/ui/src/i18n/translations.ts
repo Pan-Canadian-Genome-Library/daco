8443ff58d80a43173194af9b683645aa2f49f472
JamesTLopez <JamesTLopez.code@gmail.com>
2025-01-29 12:39:45 -0500

    feat(i18n): apply custom translations on UI

--- apps/ui/src/i18n/translations.ts
+++ apps/ui/src/i18n/translations.ts
@@ -19,26 +19,62 @@
 
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { z } from 'zod';
 
+import enFormErrors from './locale/en/enFormErrors.json';
 import enApplicationSection from './locale/en/enSection.json';
 import enGeneralLang from './locale/en/enTranslations.json';
+import frFormErrors from './locale/fr/frFormErrors.json';
 import frGeneralLang from './locale/fr/frTranslations.json';
 
-// TODO: French language is generated using online tools, replace with correct translations.
-export const resources = {
-	en: {
-		translation: { ...enGeneralLang, ...enApplicationSection },
-	},
-	fr: {
-		translation: { ...frGeneralLang },
-	},
-};
-
 i18n.use(initReactI18next).init({
-	resources,
+	resources: {
+		en: {
+			translation: {
+				...enGeneralLang,
+				...enApplicationSection,
+				...enFormErrors,
+			},
+		},
+		fr: {
+			translation: {
+				...frGeneralLang,
+				...frFormErrors,
+			},
+		},
+	},
 	lng: 'en',
 	fallbackLng: 'en',
 	supportedLngs: ['en', 'fr'],
 });
+
+/**
+ * Translation error mapping for custom error messages
+ */
+const CustomFormErrorTranslationMapping: z.ZodErrorMap = (error, ctx) => {
+	/**
+	 * Add translations through here
+	 */
+	switch (error.code) {
+		case z.ZodIssueCode.invalid_type:
+			if (error.expected === 'string') {
+				return { message: i18n.t('requiredField') };
+			}
+			break;
+		case z.ZodIssueCode.invalid_string:
+			if (error.validation === 'email') {
+				return { message: i18n.t('validEmail') };
+			} else if (error.validation === 'url') {
+				return { message: i18n.t('validURL') };
+			} else if (error.validation === 'regex') {
+				// right now this only does the postal code, when we expand 'regex' outside of postal code, we need to adjust here
+				return { message: i18n.t('validPostalCode') };
+			}
+			break;
+	}
+	return { message: ctx.defaultError };
+};
+
+z.setErrorMap(CustomFormErrorTranslationMapping);
 
 export default i18n;
