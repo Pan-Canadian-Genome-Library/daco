570ced227018fba7143c3d967baaf64ea81792e4
Suraj <4347494+surajgoraya@users.noreply.github.com>
2025-02-04 15:20:29 -0500

    feat: ✨ requested studies page
    Adds validation + creates the page itself

--- apps/ui/src/pages/AppRouter.tsx
+++ apps/ui/src/pages/AppRouter.tsx
@@ -28,6 +28,7 @@ import Introduction from '@/pages/applications/sections/intro';
 import DashboardPage from '@/pages/dashboard';
 import HomePage from '@/pages/index';
 import ManageApplicationsPage from '@/pages/manage/applications';
+import RequestedStudy from './applications/sections/requestedStudy';
 
 export const ApplicationSectionRoutes = [
 	{
@@ -49,6 +50,11 @@ export const ApplicationSectionRoutes = [
 		route: 'collaborators',
 		path: 'collaborators/edit?',
 		element: <Collaborators />,
+	},
+	{
+		route: 'study',
+		path: 'study/edit?',
+		element: <RequestedStudy />,
 	},
 ];
 
