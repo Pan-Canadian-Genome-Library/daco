8e45a46c1f375050046adc56abf2d7274f021849
Dan <ddemaria@oicr.on.ca>
2025-02-06 12:06:19 -0500

    #59: Happy-path State transitions (#153)
    * First Action Service definition
    
    * Type updates
    
    * Initial Create Test
    
    * Add relation, update create application
    
    * Reorder swagger endpoints
    
    * Scaffold service structure
    
    * Update State values
    
    * Add comment re: action creation
    
    * Updated args
    
    * Populate all tests
    
    * Remove Base Drizzle Tests
    
    * Db connection tests, Remove model tests
    
    * Add get methods, update comments and names
    
    * Add listActions & action types
    
    * Add Unit Tests
    
    * Update actions
    
    * DB Action migration & DBML
    
    * Revised db updates w/ tests + service changes
    
    * Snapshot changes
    
    * Finalized Tests
    
    * Renamed Request Rep Action
    
    * Snapshot commit
    
    * Slim down service
    
    * Update schema dbml
    
    * Separate Sorting Methods
    
    * Additional Create API change w/ new test + test fixes
    
    * Remove describe.only
    
    * Named service exports
    
    * Prefer shortened names
    
    * Add test for sorting by id
    
    * Test for created_at sort
    
    * Remove extra 'create' action and move unit test
    
    * Renamed Actions Table w/ Migration
    
    * Drizzle Snapshot
    
    * DBML Update to reflect Actions rename
    
    * Additional Name Updates
    
    * Document Drizzle Issues
    
    * Rename files; first example DB transactions
    
    * Updated draftSubmit Test/Method Behavior
    
    * Institute Rep DB additions
    
    * DAC Approval Updates
    
    * Refactored _updateRecords method
    
    * Additional merge changes
    
    * Additional Unit Test merge fix
    
    * Remove empty lines
    
    * Add 'edit' action, multiple test/state manager updates
    
    * Drizzle commit
    
    * Remove test logging
    
    * Add explicit action types
    
    * Change applicationApi to applicationController
    
    * Explicit Types (test broken)
    
    * Tests / Types fixed
    
    * Remove Empty Lines
    
    * Add TS Docs, use AsyncResult
    
    * Update comment descriptions
    
    * Format JSDoc comments
    
    * Refactored reusable state manager actions
    
    * Remove JSDoc return types for brevity + clarity
    
    * Move type definition, improve comment
    
    * Modified comments
    
    * Add Type applicationStateTotals
    
    * Fix type build issue for applicationStateTotals
    
    * Rename api folder
    
    * Linting, Type correction
    
    * Fix Build Error
    
    * Alias import
    
    * Remove extra 'type' in var name
    
    * Remove migration and revise Edit methods
    
    * Update comment
    
    * Revise transitions for 'edit' -- no state change
    
    * Change Type Names: use model for insert, record for select
    
    * Remove application api, add Type imports
    
    * Improved applicationAction comments
    
    * Improve Application Service comments, clean action service comments
    
    * Shared AppList type
    
    * Additional Type fix
    
    * Remove 'create' actions
    
    * Auto-snapshot commit
    
    * Refactor use of transactions
    
    * Complete TODO - add schema types to exported PostgresDB type
    
    * Separate actions for approve/reject/submit from different starting states
    
    ---------
    
    Co-authored-by: Jon Eubank <joneubank@gmail.com>

---
+++ apps/api/drizzle/0002_remove_create_actions.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "public"."application_actions" ALTER COLUMN "action" SET DATA TYPE text;--> statement-breakpoint
+DROP TYPE "public"."application_action_types";--> statement-breakpoint
+CREATE TYPE "public"."application_action_types" AS ENUM('WITHDRAW', 'CLOSE', 'SUBMIT_DRAFT', 'INSTITUTIONAL_REP_REVISION_REQUEST', 'INSTITUTIONAL_REP_SUBMIT', 'INSTITUTIONAL_REP_APPROVED', 'DAC_REVIEW_REVISION_REQUEST', 'DAC_REVIEW_SUBMIT', 'DAC_REVIEW_APPROVED', 'DAC_REVIEW_REJECTED', 'REVOKE');--> statement-breakpoint
+ALTER TABLE "public"."application_actions" ALTER COLUMN "action" SET DATA TYPE "public"."application_action_types" USING "action"::"public"."application_action_types";
