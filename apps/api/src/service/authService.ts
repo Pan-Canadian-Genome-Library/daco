f0491575e4ac13992661b8773e82c65f2d276389
Jon Eubank <joneubank@gmail.com>
2025-02-16 14:37:56 -0500

    Create function to determine user role from session info

---
+++ apps/api/src/service/authService.ts
@@ -0,0 +1,12 @@
+import { userRoles, type UserRole } from '@pcgl-daco/validation';
+import type { SessionData } from 'express-session';
+
+/**
+ * Based on user data stored in session data, determine the user's role.
+ * TODO: Use actual permissions stored in session to determine user role
+ *
+ * This is temporarily returning only `APPLICANT` or `ANONYMOUS`
+ */
+export function getUserRole(session: Partial<SessionData>): UserRole {
+	return session.user ? userRoles.Values.APPLICANT : userRoles.Values.ANONYMOUS;
+}
