f765f5edb3c802bc25fc58564b2d0e82dc049d1a
JamesTLopez <JamesTLopez.code@gmail.com>
2025-03-27 12:20:07 -0400

    refactor(buffer): refactor with buffer solution

--- apps/api/src/service/fileService.ts
+++ apps/api/src/service/fileService.ts
@@ -35,13 +35,11 @@ import { PostgresTransaction, type FilesModel, type JoinedApplicationRecord } fr
 const filesSvc = (db: PostgresDb) => ({
 	getFileById: async ({
 		fileId,
-		withBuffer,
 		transaction,
 	}: {
 		fileId: number;
-		withBuffer: boolean;
 		transaction?: PostgresTransaction;
-	}): AsyncResult<Partial<FilesModel & { id: number }>> => {
+	}): AsyncResult<FilesModel & { id: number }> => {
 		const dbTransaction = transaction ? transaction : db;
 
 		const result = await dbTransaction.transaction(async (transaction) => {
@@ -53,7 +51,7 @@ const filesSvc = (db: PostgresDb) => ({
 					filename: files.filename,
 					submitter_user_id: files.submitter_user_id,
 					submitted_at: files.submitted_at,
-					...(withBuffer ? { content: files.content } : {}),
+					content: files.content,
 				})
 				.from(files)
 				.where(eq(files.id, fileId));
