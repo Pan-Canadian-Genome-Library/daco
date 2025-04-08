d204e6e6f1dd4c5dc587ba645f759675b07c3b7e
JamesTLopez <JamesTLopez.code@gmail.com>
2025-04-08 11:14:01 -0400

    feat(email): send mail functionality

--- apps/api/src/service/email/index.ts
+++ apps/api/src/service/email/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
  *
  * This program and the accompanying materials are made available under the terms of
  * the GNU Affero General Public License v3.0. You should have received a copy of the
@@ -17,12 +17,15 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// const logger = BaseLogger.forModule('emailService');
+import { getEmailConfig } from '@/config/emailConfig.ts';
+import { createTransport } from 'nodemailer';
 
-const emailSvc = () => ({
-	sendEmail: async () => {
-		console.log('Email Service Init');
-	},
-});
+const config = getEmailConfig();
 
-export { emailSvc };
+const emailClient = createTransport({
+	host: config.email.host,
+	port: config.email.port,
+	// TODO: fix this type
+} as any);
+
+export default emailClient;
