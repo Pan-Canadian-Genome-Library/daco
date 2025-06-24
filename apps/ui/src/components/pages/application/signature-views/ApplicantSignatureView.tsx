d4c1125fc5ccbf579bad88c21947782eb85ac7d7
James Lopez <jamestlopez.code@gmail.com>
2025-06-24 10:07:32 -0400

    #424: Sign and Submit Section Refactor (#429)
    * fix(download): relocate download pdf button
    
    * refactor(sign): pull out sign and submit logic to custom hook
    
    * chore(comment): update comments
    
    * feat(sign): applicant view
    
    * feat(sign): rep view
    
    * feat(sign): dac signature view init
    
    * feat(sign): signature viewer implementation
    
    * fix(i18n): add translation for views
    
    * chore(imports): use alias imports
    
    * chore(sidemenu): spelling
    
    * chore(type): more accurate typing for text props
    
    * trigger build

---
+++ apps/ui/src/components/pages/application/signature-views/ApplicantSignatureView.tsx
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import ESignature from '@/components/pages/application/form-components/ESignature';
+import SectionContent from '@/components/pages/application/SectionContent';
+import SectionFooter from '@/components/pages/application/SectionFooter';
+import SectionTitle from '@/components/pages/application/SectionTitle';
+import { useSignatureForm } from '@/components/pages/application/utils/useSignatureForm';
+import { SignatureDTO } from '@pcgl-daco/data-model';
+import { Col, Row } from 'antd';
+import { useTranslation } from 'react-i18next';
+
+type Props = {
+	signatureData?: SignatureDTO;
+	signatureLoading: boolean;
+	setOpenModal: (bool: boolean) => void;
+};
+
+const ApplicantSignatureView = ({ signatureData, signatureLoading, setOpenModal }: Props) => {
+	const { t: translate } = useTranslation();
+
+	const { form, disableSignature, disableSubmit, signatureRef, onSaveClicked } = useSignatureForm({
+		signatureData: signatureData,
+	});
+	const { control, setValue, formState, watch, clearErrors, reset } = form;
+
+	const watchSignature = watch('signature');
+
+	return (
+		<>
+			<SectionTitle
+				title={translate('sign-and-submit-section.title')}
+				showLockIcon={disableSignature}
+				text={translate('sign-and-submit-section.description')}
+				showDivider={false}
+			/>
+			<SectionContent
+				showDivider={false}
+				title={translate('sign-and-submit-section.section.title')}
+				text={translate('sign-and-submit-section.section.description')}
+			>
+				<Row>
+					<Col xs={{ flex: '100%' }} md={{ flex: '100%' }} lg={{ flex: '100%' }}>
+						<input disabled type="hidden" name="createdAt" />
+						{!signatureLoading ? (
+							<ESignature
+								disabled={disableSignature}
+								signatureRef={signatureRef}
+								name="signature"
+								control={control}
+								watch={watch}
+								formState={formState}
+								setValue={setValue}
+								reset={reset}
+								clearErrors={clearErrors}
+								disableSaveButton={!watchSignature || disableSignature}
+								onSaveClicked={onSaveClicked}
+								saveButtonText={translate('sign-and-submit-section.section.buttons.save')}
+								clearButtonText={translate('sign-and-submit-section.section.buttons.clear')}
+							/>
+						) : null}
+					</Col>
+				</Row>
+				<Row style={{ minHeight: '40vh' }} />
+			</SectionContent>
+			<SectionFooter
+				currentRoute="sign"
+				isEditMode={disableSubmit}
+				signSubmitHandler={() => {
+					setOpenModal(true);
+				}}
+				submitDisabled={disableSubmit}
+			/>
+		</>
+	);
+};
+
+export default ApplicantSignatureView;
---
+++ apps/ui/src/components/pages/application/signature-views/RepSignatureView.tsx
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import ESignature from '@/components/pages/application/form-components/ESignature';
+import SectionContent from '@/components/pages/application/SectionContent';
+import SectionFooter from '@/components/pages/application/SectionFooter';
+import SectionTitle from '@/components/pages/application/SectionTitle';
+import SignatureViewer from '@/components/pages/application/SignatureViewer';
+import { useSignatureForm } from '@/components/pages/application/utils/useSignatureForm';
+import TextList from '@/components/TextList';
+import { useApplicationContext } from '@/providers/context/application/ApplicationContext';
+import { SignatureDTO } from '@pcgl-daco/data-model';
+import { Col, Flex, Row, Typography } from 'antd';
+import { Trans, useTranslation } from 'react-i18next';
+
+const { Text } = Typography;
+
+type Props = {
+	signatureData?: SignatureDTO;
+	signatureLoading: boolean;
+	setOpenModal: (bool: boolean) => void;
+};
+
+const RepSignatureView = ({ signatureData, signatureLoading, setOpenModal }: Props) => {
+	const { t: translate } = useTranslation();
+	const {
+		state: { fields },
+	} = useApplicationContext();
+
+	const { applicantFirstName, applicantLastName } = fields;
+
+	const { form, disableSignature, disableSubmit, signatureRef, onSaveClicked } = useSignatureForm({
+		signatureData: signatureData,
+	});
+	const { control, setValue, formState, watch, clearErrors, reset } = form;
+
+	const watchSignature = watch('signature');
+
+	const PointArray = [
+		<Trans i18nKey={'sign-and-submit-section.rep-section.point1'} components={{ bold: <strong /> }} />,
+		<Trans i18nKey={'sign-and-submit-section.rep-section.point2'} components={{ bold: <strong /> }} />,
+	];
+
+	return (
+		<>
+			<SectionTitle
+				title={translate('sign-and-submit-section.title')}
+				showLockIcon={disableSignature}
+				showDivider={false}
+			>
+				<Flex vertical>
+					<Text>{translate('sign-and-submit-section.rep-section.desc1')}</Text>
+					<TextList data={PointArray} />
+				</Flex>
+			</SectionTitle>
+			<SectionContent showDivider={true}>
+				{!signatureLoading ? (
+					<SignatureViewer
+						title="Applicant"
+						name={`${applicantFirstName} ${applicantLastName}`}
+						signature={signatureData?.applicantSignature}
+						date={signatureData?.applicantSignedAt}
+					/>
+				) : null}
+			</SectionContent>
+			<SectionContent
+				title={translate('sign-and-submit-section.section.title')}
+				text={translate('sign-and-submit-section.section.description')}
+			>
+				<Row>
+					<Col xs={{ flex: '100%' }} md={{ flex: '100%' }} lg={{ flex: '100%' }}>
+						<input disabled type="hidden" name="createdAt" />
+						{!signatureLoading ? (
+							<ESignature
+								disabled={disableSignature}
+								signatureRef={signatureRef}
+								name="signature"
+								control={control}
+								watch={watch}
+								formState={formState}
+								setValue={setValue}
+								reset={reset}
+								clearErrors={clearErrors}
+								disableSaveButton={!watchSignature || disableSignature}
+								onSaveClicked={onSaveClicked}
+								saveButtonText={translate('sign-and-submit-section.section.buttons.save')}
+								clearButtonText={translate('sign-and-submit-section.section.buttons.clear')}
+							/>
+						) : null}
+					</Col>
+				</Row>
+				<Row style={{ minHeight: '40vh' }} />
+			</SectionContent>
+			<SectionFooter
+				currentRoute="sign"
+				isEditMode={disableSubmit}
+				signSubmitHandler={() => {
+					setOpenModal(true);
+				}}
+				submitDisabled={disableSubmit}
+			/>
+		</>
+	);
+};
+
+export default RepSignatureView;
