700688f675c8a8a3330d1756ae739616c051baac
James Lopez <jamestlopez.code@gmail.com>
2025-07-28 09:28:31 -0400

    fix(email): make daco a link

--- apps/api/src/service/email/layouts/templates/EmailInstitutionalRepReview.ts
+++ apps/api/src/service/email/layouts/templates/EmailInstitutionalRepReview.ts
@@ -41,7 +41,7 @@ export const GenerateEmailInstitutionalRepReview = ({
                     We hope this message finds you well.
                 </mj-text>
                 <mj-text>
-                   <b>${applicantName} has identified you as the institutional representative of the PCGL DACO application. The application has been submitted and is now awaiting your review.</b> Please login to the DACO portal using your institutional email and review the application. Your timely input and approval are essential for the next steps in the process.         
+                   <b>${applicantName} has identified you as the institutional representative of the<a href="${ui}" target="_blank" rel="nofollow"> PCGL DACO application</a>. The application has been submitted and is now awaiting your review.</b> Please login to the DACO portal using your institutional email and review the application. Your timely input and approval are essential for the next steps in the process.         
                 </mj-text>
                 <mj-text>
                     Here are the details of the application:
