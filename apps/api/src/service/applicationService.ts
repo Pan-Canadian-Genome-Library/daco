e85a773a84b21b6aed5d0da92f7d8164d3861c66
James Lopez <jamestlopez.code@gmail.com>
2025-11-07 14:21:17 -0500

    fix(search): use partial matching query instead

--- apps/api/src/service/applicationService.ts
+++ apps/api/src/service/applicationService.ts
@@ -281,17 +281,19 @@ const applicationSvc = (db: PostgresDb) => ({
 			const transformSearchIntoQuery = (searchText: string) => {
 				const sanitizedSearch = searchText
 					?.trim()
-					.replace(/[^a-zA-Z0-9\s]/g, '') // Remove special characters
+					.replace(/[^a-zA-Z0-9\s@.\-]/g, '') // Remove special characters except @ . -
 					.trim() // apply trim again in-case user inputs a special characters as the first/last word
 					.replace(/\s+/g, ' | '); // add OR between phrases
 
-				const searchQuery = sql`(      
-					setweight(to_tsvector('english', ${applicationContents.application_id}::text), 'A') ||
-					setweight(to_tsvector('english', COALESCE(${applicationContents.applicant_first_name}, '') || ' ' || COALESCE(${applicationContents.applicant_last_name}, '')), 'B') ||			    
-					setweight(to_tsvector('english', COALESCE(${applicationContents.applicant_institutional_email}, '')), 'C') ||
-					setweight(to_tsvector('english', COALESCE(${applicationContents.applicant_primary_affiliation}, '')), 'D')
-				)
-     		 @@ to_tsquery('english', ${sanitizedSearch + ':*'})`;
+				let searchQuery;
+				// Use ILIKE for better partial matching
+				const search = `%${sanitizedSearch}%`;
+				searchQuery = sql`(
+						${applicationContents.application_id}::text ILIKE ${search} OR
+						(COALESCE(${applicationContents.applicant_first_name}, '') || ' ' || COALESCE(${applicationContents.applicant_last_name}, '')) ILIKE ${search} OR
+						COALESCE(${applicationContents.applicant_institutional_email}, '') ILIKE ${search} OR
+						COALESCE(${applicationContents.applicant_primary_affiliation}, '') ILIKE ${search}
+					)`;
 
 				return searchQuery;
 			};
