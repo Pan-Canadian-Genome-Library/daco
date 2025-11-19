f2b57a2523f999ae8544f2a54b6059ae725077cb
James Lopez <jamestlopez.code@gmail.com>
2025-11-19 09:37:59 -0500

    #475: Manage Applications Search Bar Implemenation (#485)
    * chore(func): move functions to manageUtils.ts
    
    * feat(search): search functionality
    
    * feat(search): remove special characters and sanitize logic
    
    * feat(search): ui integration of search
    
    * fix(search): Allow null values to return for id search
    
    * fix(searc): remove prefix PCGL- if added
    
    * refactor(totals): move ApplicationStateTotals to sharedpackage
    
    * fix(shared): update shared package for ApplicationListResponse
    
    * fix(search): retire counts endpoint ui
    
    * feat(getAllApplications): include count in result
    
    * refactor(counts): remove applicationCounts from api
    
    * fix(search): resolve multiple endpoint calls
    
    * feat(search-ui): add translations and search text component
    
    * fix(swagger): update swagger to include search
    
    * fix(test): remove applicationStateTotals test cases
    
    * chore(comment): copyright
    
    * fix(count): resolve counts not working
    
    * fix(useState): remove redundant useState
    
    * fix(search): use partial matching query instead
    
    * fix(search): update sanitized search
    
    * fix(ts): add docts for search
    
    * revert(search): use previous search solution
    
    * chore(debug): remove debug comments
    
    * fix(totals): resolve bug where selecting totals doesnt reset
    
    * refactor(counts): use TOTAL from totals and remove redudant types
    
    * Update apps/ui/src/pages/manage/applications/index.tsx
    
    Co-authored-by: Jon Eubank <joneubank@gmail.com>
    
    * fix(manage): remove useless condition
    
    ---------
    
    Co-authored-by: Jon Eubank <joneubank@gmail.com>

--- packages/data-model/src/types.ts
+++ packages/data-model/src/types.ts
@@ -193,7 +193,6 @@ export interface ApplicationResponseData extends ApplicationDTO {
 }
 
 export interface PagingMetadata {
-	totalRecords: number;
 	page: number;
 	pageSize: number;
 }
@@ -209,11 +208,6 @@ export interface ApplicantSummary {
 
 export interface ApplicationListSummary extends ApplicationDTO {
 	applicant: ApplicantSummary | null;
-}
-
-export interface ApplicationListResponse {
-	applications: ApplicationListSummary[];
-	pagingMetadata: PagingMetadata;
 }
 
 export type ApplicationContentsResponse = {
@@ -233,12 +227,6 @@ export interface ApplicationResponseData extends ApplicationDTO {
 	contents: ApplicationContentsResponse | null;
 }
 
-export interface PagingMetadata {
-	totalRecords: number;
-	page: number;
-	pageSize: number;
-}
-
 export interface ApplicantSummary {
 	createdAt: Date;
 	firstName: string | null;
@@ -252,9 +240,23 @@ export interface ApplicationListSummary extends ApplicationDTO {
 	applicant: ApplicantSummary | null;
 }
 
+export type ApplicationStateTotals = {
+	APPROVED: number;
+	CLOSED: number;
+	DAC_REVIEW: number;
+	DAC_REVISIONS_REQUESTED: number;
+	DRAFT: number;
+	INSTITUTIONAL_REP_REVIEW: number;
+	REJECTED: number;
+	INSTITUTIONAL_REP_REVISION_REQUESTED: number;
+	REVOKED: number;
+	TOTAL: number;
+};
+
 export interface ApplicationListResponse {
 	applications: ApplicationListSummary[];
 	pagingMetadata: PagingMetadata;
+	totals: ApplicationStateTotals;
 }
 
 export type ApproveApplication = {
