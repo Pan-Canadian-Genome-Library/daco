62f93d360ac44ed9a6df4d8147b5c880798ff9e2
James Lopez <jamestlopez.code@gmail.com>
2025-12-01 10:59:40 -0500

    #480, #481: Submit DAC Comments API Implementation (#504)
    * feat(dac): add dac_chair and update env variables
    
    * fix(dac): change revisions permissions and code cleanup
    
    * chore(dac): update swagger and naming in endpoints
    
    * chore(dac): init comment endpoints and update comments
    
    * fix(dac): resolve missing dac chair permissions
    
    * feat(dac): dac chair permissions on UI
    
    * fix(dac): resolve dac member still can approve reject
    
    * feat(comments): comments db changes
    
    * fix(placeholder): update placeholder status code
    
    * fix(naming): update naming and formatting
    
    * fix(naming): update file naming
    
    * feat(dac): add dac comments generate
    
    * fix(tag): update tag
    
    * fix(package): use SectionRoutes to populate sectionsEnum
    
    * fix(uppercase): to uppercase for sectionEnumes
    
    * feat(submitComment): create submit comment endpoint
    
    * feat(api-get-comments): get comments by id and section
    
    * feat(swagger): add swagger  for get comments
    
    * fix(api-get-comment): prevent other  applicants seeing comments
    
    * feat(test): add tests cases for comments
    
    * fix(api-dac-comments): return dacChairOnly comments if user made it
    
    * fix(types): resolve snakecase type and shorter min in zod
    
    * fix(api): resolve feedback

--- apps/api/tests/services/application-service.test.ts
+++ apps/api/tests/services/application-service.test.ts
@@ -378,6 +378,125 @@ describe('Application Service', () => {
 		});
 	});
 
+	describe('Get Application Metadata', () => {
+		it('should list statistics for how many applications are in each state category', async () => {
+			const appStateTotals = await testApplicationService.applicationStateTotals();
+			assert.ok(appStateTotals.success);
+
+			const allStates = appStateTotals.data;
+
+			const allApplications = await testApplicationService.listApplications({
+				user_id,
+				pageSize: allRecordsPageSize,
+			});
+			assert.ok(allApplications.success);
+
+			const applicationRecords = allApplications.data.applications;
+
+			assert.ok(Array.isArray(applicationRecords));
+			assert.ok(allStates);
+
+			const allDraftRecords = applicationRecords.filter((records) => records.state === 'DRAFT');
+
+			assert.equal(allStates.DRAFT, allDraftRecords.length);
+			assert.equal(allStates.TOTAL, applicationRecords.length);
+		});
+	});
+
+	describe('Application Comments', () => {
+		const testApplicationId = 4;
+		// Create test data
+		before(async () => {
+			await testApplicationService.createDacComment({
+				applicationId: testApplicationId,
+				section: 'intro',
+				message: 'Intro comment',
+				toDacChair: false,
+				userId: 'TEST-003',
+				userName: 'Jeff',
+			});
+			await testApplicationService.createDacComment({
+				applicationId: testApplicationId,
+				section: 'intro',
+				message: 'Intro comment again',
+				toDacChair: false,
+				userId: 'TEST-003',
+				userName: 'Jeff',
+			});
+			await testApplicationService.createDacComment({
+				applicationId: testApplicationId,
+				section: 'project',
+				message: 'Trying this out',
+				toDacChair: false,
+				userId: 'TEST-005',
+				userName: 'Mina',
+			});
+			await testApplicationService.createDacComment({
+				applicationId: testApplicationId,
+				section: 'project',
+				message: 'Lispsum',
+				toDacChair: true,
+				userId: 'TEST-005',
+				userName: 'Mina',
+			});
+		});
+
+		it('should create a dac comment and also retrieve created comment', async () => {
+			const result = await testApplicationService.createDacComment({
+				applicationId: 1,
+				section: 'project',
+				message: 'Hello test comment',
+				toDacChair: false,
+				userId: 'TEST-001',
+				userName: 'Julia',
+			});
+
+			assert.ok(result.success);
+			const dacCommentResult = await testApplicationService.getDacComment({
+				applicationId: 1,
+				section: 'project',
+				isDac: false,
+			});
+
+			assert.ok(dacCommentResult.success);
+			assert.ok(Array.isArray(dacCommentResult.data) && dacCommentResult.data.length > 0);
+			assert.ok(dacCommentResult.data[0]?.applicationId === 1);
+		});
+
+		it('should only retrieve the intro', async () => {
+			const dacCommentResult = await testApplicationService.getDacComment({
+				applicationId: testApplicationId,
+				section: 'intro',
+				isDac: false,
+			});
+
+			assert.ok(dacCommentResult.success);
+			assert.ok(Array.isArray(dacCommentResult.data) && dacCommentResult.data.length === 2);
+		});
+
+		it('should retrieve dac comments if dac_chair_only is false', async () => {
+			const dacCommentResult = await testApplicationService.getDacComment({
+				applicationId: testApplicationId,
+				section: 'project',
+				isDac: false,
+			});
+
+			assert.ok(dacCommentResult.success);
+			assert.ok(Array.isArray(dacCommentResult.data) && dacCommentResult.data.length === 1);
+		});
+
+		it('should retrieve all dac comments regardless dac_chair_only because user is DAC', async () => {
+			const dacCommentResult = await testApplicationService.getDacComment({
+				applicationId: testApplicationId,
+				section: 'project',
+				isDac: true,
+			});
+
+			assert.ok(dacCommentResult.success);
+			assert.ok(Array.isArray(dacCommentResult.data) && dacCommentResult.data.length === 2);
+		});
+	});
+
 	after(async () => {
 		await container.stop();
 		process.exit(0);
