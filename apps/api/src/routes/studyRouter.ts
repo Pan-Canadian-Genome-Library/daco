f5c27cc2bdd244f4e69f2c2e72ebe1d331d579ff
James Lopez <jamestlopez.code@gmail.com>
2026-01-21 13:58:18 -0500

    feat(study): add study routes,controller,service getById

---
+++ apps/api/src/routes/studyRouter.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2026 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import type { StudyDTO } from '@pcgl-daco/data-model';
+import { withParamsSchemaValidation } from '@pcgl-daco/request-utils';
+import express from 'express';
+
+import { getStudyById } from '@/controllers/studyController.ts';
+import { authMiddleware } from '@/middleware/authMiddleware.ts';
+import { apiZodErrorMapping } from '@/utils/validation.js';
+import { basicStudyParamSchema } from '@pcgl-daco/validation';
+import type { ResponseWithData } from './types.ts';
+
+const studyRouter = express.Router();
+
+/**
+ * Get the study by studyId
+ */
+studyRouter.get(
+	'/:studyId',
+	authMiddleware(),
+	withParamsSchemaValidation(
+		basicStudyParamSchema,
+		apiZodErrorMapping,
+		async (request, response: ResponseWithData<StudyDTO, ['NOT_FOUND', 'FORBIDDEN']>) => {
+			const { user } = request.session;
+			const studyId = String(request.params.studyId);
+
+			if (user && user?.siteAdmin) {
+				const result = await getStudyById({ studyId });
+
+				if (result.success) {
+					response.status(200).json(result.data);
+					return;
+				}
+			}
+
+			response.status(403).json({ error: 'FORBIDDEN', message: `User does not have permission to access this study.` });
+			return;
+		},
+	),
+);
+
+export default studyRouter;
---
+++ apps/api/src/service/studyService.ts
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2026 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { eq } from 'drizzle-orm';
+
+import { type PostgresDb } from '@/db/index.js';
+import { study } from '@/db/schemas/studies.ts';
+import BaseLogger from '@/logger.ts';
+import { failure, success, type AsyncResult } from '@/utils/results.js';
+import { StudyDTO } from '@pcgl-daco/data-model';
+import { type PostgresTransaction } from './types.ts';
+
+const logger = BaseLogger.forModule('studyService');
+
+/**
+ * Study service provides methods for study DB access
+ * @param db - Drizzle Postgres DB Instance
+ */
+const studySvc = (db: PostgresDb) => ({
+	getStudyById: async ({
+		studyId,
+		transaction,
+	}: {
+		studyId: string;
+		transaction?: PostgresTransaction;
+	}): AsyncResult<StudyDTO, 'SYSTEM_ERROR'> => {
+		const dbTransaction = transaction ? transaction : db;
+
+		try {
+			const result = await dbTransaction.transaction(async (transaction) => {
+				const studyRecord = await transaction
+					.select({
+						studyId: study.study_id,
+						dacId: study.dac_id,
+						categoryId: study.category_id,
+						studyName: study.study_name,
+						studyDescription: study.study_description,
+						programName: study.program_name,
+						keywords: study.keywords,
+						status: study.status,
+						context: study.context,
+						domain: study.domain,
+						participantCriteria: study.participant_criteria,
+						principalInvestigators: study.principal_investigators,
+						leadOrganizations: study.lead_organizations,
+						collaborators: study.collaborators,
+						fundingSources: study.funding_sources,
+						publicationLinks: study.publication_links,
+						acceptingApplications: study.accepting_applications,
+						createdAt: study.created_at,
+						updatedAt: study.updated_at,
+					})
+					.from(study)
+					.where(eq(study.study_id, studyId));
+
+				if (!studyRecord[0]) {
+					throw new Error(`Study record ${studyId} is undefined.`);
+				}
+				return studyRecord[0];
+			});
+
+			return success(result);
+		} catch (error) {
+			const message = 'Error at getStudyById';
+
+			logger.error(message, error);
+
+			return failure('SYSTEM_ERROR', message);
+		}
+	},
+});
+
+export { studySvc };
