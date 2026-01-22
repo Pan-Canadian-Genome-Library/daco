fab755bc12a67795d483c274923f7f3df0a83e8b
Dan DeMaria <ddemaria@oicr.on.ca>
2026-01-22 14:55:42 -0500

    Clean up EmailReminders

--- apps/api/src/jobs/emailReminders.ts
+++ apps/api/src/jobs/emailReminders.ts
@@ -38,21 +38,32 @@ import {
 
 const logger = BaseLogger.forModule('Email Reminders');
 
-const dateDiffCheck = ({ actionDate, interval = 7 }: { actionDate: Date | null; interval?: number }) => {
-	if (!actionDate) return false;
+// Compare a supplied email or action date (measured in Days) against an interval value
+// If days passed since action date is greater than the interval, return true, else return false
+const dateDiffCheck = ({ actionDate, interval = 7 }: { actionDate: Date; interval?: number }) => {
 	const currentDate = new Date().getDate();
 	const comparisonDate = actionDate.getDate();
 	const diff = currentDate - comparisonDate;
 	return diff >= interval;
 };
 
-const targetEmailTypes: Partial<{ [k in ApplicationStateValues]: EmailTypeValues }> = {
-	[ApplicationStates.INSTITUTIONAL_REP_REVIEW]: EmailTypes.REMINDER_SUBMIT_INSTITUTIONAL_REP_REVIEW,
+// Dictionary matching Reminder Email subjects to the triggering Application State
+// Rep Review and Dac Review states can trigger multiple Reminder Email types depending on the previous action
+const targetEmailTypes: Partial<{ [k in ApplicationStateValues]: EmailTypeValues | EmailTypeValues[] }> = {
+	[ApplicationStates.DRAFT]: EmailTypes.REMINDER_SUBMIT_DRAFT,
+	[ApplicationStates.INSTITUTIONAL_REP_REVIEW]: [
+		EmailTypes.REMINDER_SUBMIT_INSTITUTIONAL_REP_REVIEW,
+		EmailTypes.REMINDER_SUBMIT_REVISIONS_INSTITUTIONAL_REP,
+	],
 	[ApplicationStates.INSTITUTIONAL_REP_REVISION_REQUESTED]: EmailTypes.REMINDER_REQUEST_REVISIONS_INSTITUTIONAL_REP,
-	[ApplicationStates.DAC_REVIEW]: EmailTypes.REMINDER_SUBMIT_DAC_REVIEW,
+	[ApplicationStates.DAC_REVIEW]: [
+		EmailTypes.REMINDER_SUBMIT_DAC_REVIEW,
+		EmailTypes.REMINDER_SUBMIT_REVISIONS_DAC_REVIEW,
+	],
 	[ApplicationStates.DAC_REVISIONS_REQUESTED]: EmailTypes.REMINDER_REQUEST_REVISIONS_DAC_REVIEW,
 };
 
+// Find record for most recent relevant email created with target email_type value
 const getMostRecentEmail = ({
 	sentEmails,
 	state,
@@ -62,26 +73,37 @@ const getMostRecentEmail = ({
 }) => {
 	const targetEmailType = targetEmailTypes[state];
 	const mostRecentEmail =
-		(sentEmails
-			? sentEmails.length > 1
-				? sentEmails.find((email) => email.email_type === targetEmailType)
-				: sentEmails[0]
-			: null) || null;
-	return mostRecentEmail;
+		sentEmails?.length && targetEmailType
+			? sentEmails.find((email) =>
+					typeof targetEmailType === 'string'
+						? email.email_type === targetEmailType
+						: targetEmailType.includes(email.email_type),
+				)
+			: null;
+	return mostRecentEmail || null;
 };
 
-const checkForEmailReminders = ({ mostRecentEmail }: { mostRecentEmail: EmailRecord | null }) => {
-	const mostRecentEmailDate = mostRecentEmail?.created_at || null;
-	return mostRecentEmailDate && dateDiffCheck({ actionDate: mostRecentEmailDate });
+// Return true if a relevant email record exists and was created
+const checkForEmailReminders = ({ emailDate }: { emailDate: Date | null }) => {
+	return !!emailDate && dateDiffCheck({ actionDate: emailDate });
 };
 
-const targetActionTypes: Partial<{ [k in ApplicationStateValues]: ApplicationActionValues }> = {
-	[ApplicationStates.INSTITUTIONAL_REP_REVIEW]: ApplicationActions.SUBMIT_DRAFT,
+// Dictionary matching Application State values to related triggering State Transition Action values
+// Rep Review and Dac Review states can be triggered by multiple potential State Transitions
+const targetActionTypes: Partial<{
+	[k in ApplicationStateValues]: ApplicationActionValues | ApplicationActionValues[];
+}> = {
+	[ApplicationStates.DRAFT]: ApplicationActions.WITHDRAW,
+	[ApplicationStates.INSTITUTIONAL_REP_REVIEW]: [
+		ApplicationActions.SUBMIT_DRAFT,
+		ApplicationActions.INSTITUTIONAL_REP_SUBMIT,
+	],
 	[ApplicationStates.INSTITUTIONAL_REP_REVISION_REQUESTED]: ApplicationActions.INSTITUTIONAL_REP_REVISION_REQUEST,
-	[ApplicationStates.DAC_REVIEW]: ApplicationActions.INSTITUTIONAL_REP_SUBMIT,
+	[ApplicationStates.DAC_REVIEW]: [ApplicationActions.INSTITUTIONAL_REP_SUBMIT, ApplicationActions.DAC_REVIEW_SUBMIT],
 	[ApplicationStates.DAC_REVISIONS_REQUESTED]: ApplicationActions.DAC_REVIEW_REVISION_REQUEST,
 };
 
+// Find record for most recent relevant Action created with target state transition Action value
 const getMostRecentAction = ({
 	applicationActions,
 	state,
@@ -91,19 +113,21 @@ const getMostRecentAction = ({
 }) => {
 	const targetActionType = targetActionTypes[state];
 	const mostRecentAction =
-		(applicationActions
-			? applicationActions.length > 1
-				? applicationActions.find((action) => action.action === targetActionType)
-				: applicationActions[0]
-			: null) || null;
-	return mostRecentAction;
+		applicationActions?.length && targetActionType
+			? applicationActions.find((action) =>
+					typeof targetActionType === 'string'
+						? action.action === targetActionType
+						: targetActionType.includes(action.action),
+				)
+			: null;
+	return mostRecentAction || null;
 };
 
-const checkForActionReminders = ({ mostRecentAction }: { mostRecentAction: ApplicationActionRecord | null }) => {
-	const mostRecentActionDate = mostRecentAction?.created_at || null;
-	return mostRecentActionDate && dateDiffCheck({ actionDate: mostRecentActionDate });
+const checkForActionReminders = ({ actionDate }: { actionDate: Date | null }) => {
+	return !!actionDate && dateDiffCheck({ actionDate });
 };
 
+// Reviews Application, Action & Email details to determine if an Application needs a Reminder Email
 export const scheduleEmailReminders = async () => {
 	const database = getDbInstance();
 	const applicationService = applicationSvc(database);
@@ -120,64 +144,61 @@ export const scheduleEmailReminders = async () => {
 	if (allApplicationsResult.success) {
 		const applications = allApplicationsResult.data;
 		for (const application of applications) {
-			const {
-				state,
-				created_at: createdAt,
-				application_actions: applicationActions,
-				sent_emails: sentEmails,
-			} = application;
+			const { state, created_at, application_actions: applicationActions, sent_emails: sentEmails } = application;
 
-			if (state === ApplicationStates.DRAFT) {
-				// TODO: Handle Withdrawn vs New Application
-				// If Application is still in DRAFT 7 days after creation, send a reminder email
-				const sendReminder = dateDiffCheck({ actionDate: createdAt });
-				if (sendReminder) {
-					sendEmailReminders({
-						application,
-						action: null,
-					});
-				}
-			} else {
-				// For all other states, confirm if it has been >7 days since the last related reminder email or state change
-				const mostRecentEmail = getMostRecentEmail({ state, sentEmails });
-				const needsEmailReminder = checkForEmailReminders({ mostRecentEmail });
-				const mostRecentAction = getMostRecentAction({ state, applicationActions });
-				const needsStateReminder = checkForActionReminders({ mostRecentAction });
-				const sendReminder = needsEmailReminder || needsStateReminder;
-				if (sendReminder) {
-					sendEmailReminders({
-						application,
-						action: mostRecentAction,
-					});
-				}
+			// Early Draft applications will not have any state transition Action records
+			// If Application is in DRAFT 7 days after creation (with no previous Actions), check created_at Date
+			// For all other states, confirm if it has been >7 days since the last related state change or reminder email
+			const mostRecentAction = getMostRecentAction({ state, applicationActions });
+			const actionDate = mostRecentAction
+				? mostRecentAction.created_at
+				: state === ApplicationStates.DRAFT
+					? created_at
+					: null;
+			const needsStateReminder = checkForActionReminders({ actionDate });
+
+			const mostRecentEmail = getMostRecentEmail({ state, sentEmails });
+			const emailDate = mostRecentEmail?.created_at || null;
+			const needsEmailReminder = checkForEmailReminders({ emailDate });
+
+			const sendReminder = needsEmailReminder || needsStateReminder;
+			if (sendReminder) {
+				sendEmailReminders({
+					application,
+					relatedAction: mostRecentAction,
+					relatedEmail: mostRecentEmail,
+				});
 			}
 		}
 	} else {
-		console.log(allApplicationsResult);
-		throw new Error('Error retrieving applications');
+		const { error, message } = allApplicationsResult;
+		logger.error(message, error);
+		throw new Error(message);
 	}
 };
 
+// Generates & Sends Reminder Emails based on Application State
 export const sendEmailReminders = ({
 	application,
-	action,
+	relatedAction,
 }: {
 	application: JoinedApplicationEmailsActionsRecord;
-	action: ApplicationActionRecord | null;
+	relatedAction?: ApplicationActionRecord | null;
+	relatedEmail?: EmailRecord | null;
 }) => {
 	const database = getDbInstance();
 	const emailService = emailSvc(database);
 	const { application_id, state, created_at, application_contents } = application;
-	const { id: application_action_id, created_at: actionDate, user_name, user_id } = action || {};
+	const { id: application_action_id, created_at: actionDate, user_name, user_id } = relatedAction || {};
 
 	if (state === ApplicationStates.DRAFT) {
+		// If in State Draft for over 7 days, send a reminder email to Submit
 		const { applicant_first_name, applicant_last_name, applicant_institutional_email } = application_contents || {};
 
 		const applicantName = applicant_first_name
 			? `${applicant_first_name} ${applicant_last_name || ''}`.trim()
 			: 'Applicant';
 
-		// if still in draft after 7 days -> send email reminder
 		emailService.sendEmailSubmitDraftReminder({
 			id: application_id,
 			actionId: application_action_id,
@@ -186,8 +207,9 @@ export const sendEmailReminders = ({
 			to: applicant_institutional_email,
 		});
 	} else if (
-		application_contents !== null &&
-		action !== null &&
+		// All post-Draft Applications should have Application Contents and at least one Action record
+		!!application_contents &&
+		!!relatedAction &&
 		actionDate !== undefined &&
 		application_action_id !== undefined
 	) {
@@ -206,38 +228,28 @@ export const sendEmailReminders = ({
 		const repName = institutional_rep_first_name
 			? `${institutional_rep_first_name} ${institutional_rep_last_name || ''}`.trim()
 			: 'Representative';
-		// TODO: Need to Define Approach for Dac Member
+
+		// TODO: Need to Define Approach for Dac Member Names & Emails
 		const dacEmail = user_id ?? 'pcgl_email@yopmail.com';
 		const dacMemberName = user_name ? user_name : 'Dac Member';
 		const submittedDate = actionDate;
 
 		switch (state) {
 			case ApplicationStates.DAC_REVIEW:
-				// Post Submit Rep Review, Application has moved to Dac Review, if still in review 7 days later -> send email reminder
-				emailService.sendEmailDacReviewReminder({
-					id: application_id,
-					applicantName,
-					actionId: application_action_id,
-					repName: dacMemberName,
-					submittedDate,
-					to: dacEmail,
-				});
-
-				break;
-			case ApplicationStates.DAC_REVISIONS_REQUESTED: {
-				// TODO: Check Action type
-				if (user_name === 'APPLICANT') {
-					// Post Dac Revisions Submitted, if still not submitted 7 days later -> send email reminder
-					emailService.sendEmailSubmitDacRevisionsReminder({
+				if (relatedAction.action === ApplicationActions.INSTITUTIONAL_REP_SUBMIT) {
+					// Post Institutional Rep Submission, Application has moved to Dac Review
+					// If still in review 7 days later -> send email reminder to Dac Member
+					emailService.sendEmailDacReviewReminder({
 						id: application_id,
-						actionId: application_action_id,
 						applicantName,
+						actionId: application_action_id,
 						repName: dacMemberName,
 						submittedDate,
-						to: applicantEmail,
+						to: dacEmail,
 					});
-				} else if (user_name === 'DAC_MEMBER') {
-					// Post Dac Revisions Requested, if still in review 7 days later -> send email reminder
+				} else if (relatedAction.action === ApplicationActions.DAC_REVIEW_SUBMIT) {
+					// Post Dac Revisions Submitted, Application has moved back to Dac Review
+					// If still in review 7 days later -> send email reminder to Dac Member
 					emailService.sendEmailDacRevisionsReminder({
 						id: application_id,
 						actionId: application_action_id,
@@ -248,31 +260,31 @@ export const sendEmailReminders = ({
 					});
 				}
 				break;
-			}
-			case ApplicationStates.INSTITUTIONAL_REP_REVIEW:
-				// Post Submit Draft, Application has moved to Rep Review, if still in review 7 days later -> send email reminder
-				emailService.sendEmailRepReviewReminder({
+			case ApplicationStates.DAC_REVISIONS_REQUESTED: {
+				// Post Dac Revisions Requested, if still not Subbmitted 7 days later -> send email reminder to Applicant
+				emailService.sendEmailSubmitDacRevisionsReminder({
 					id: application_id,
-					applicantName,
 					actionId: application_action_id,
-					repName,
+					applicantName,
+					repName: dacMemberName,
 					submittedDate,
 					to: applicantEmail,
 				});
 				break;
-			case ApplicationStates.INSTITUTIONAL_REP_REVISION_REQUESTED: {
-				// TODO: Check Action type
-				if (action.user_name === 'APPLICANT') {
-					// Post Rep Revisions Requested, if still in review 7 days later -> send Applicant email reminder
-					emailService.sendEmailSubmitRepRevisionsReminder({
+			}
+			case ApplicationStates.INSTITUTIONAL_REP_REVIEW:
+				if (relatedAction.action === ApplicationActions.SUBMIT_DRAFT) {
+					// Post Submit Draft, Application has moved to Rep Review
+					// If still in review 7 days later -> send email reminder to Institutional Rep
+					emailService.sendEmailRepReviewReminder({
 						id: application_id,
 						applicantName,
 						actionId: application_action_id,
-						submittedDate,
 						repName,
+						submittedDate,
 						to: applicantEmail,
 					});
-				} else if (action.user_name === 'INSTITUTIONAL_REP') {
+				} else if (relatedAction.action === ApplicationActions.INSTITUTIONAL_REP_SUBMIT) {
 					// Post Rep Revisions Submitted, if still in review 7 days later -> send Institutional Rep email reminder
 					emailService.sendEmailRepRevisionsReminder({
 						id: application_id,
@@ -283,7 +295,18 @@ export const sendEmailReminders = ({
 					});
 				}
 				break;
-			}
+			case ApplicationStates.INSTITUTIONAL_REP_REVISION_REQUESTED:
+				// Post Rep Revisions Requested, if still in review 7 days later -> send Applicant email reminder
+				emailService.sendEmailSubmitRepRevisionsReminder({
+					id: application_id,
+					applicantName,
+					actionId: application_action_id,
+					submittedDate,
+					repName,
+					to: applicantEmail,
+				});
+
+				break;
 			default:
 				break;
 		}
