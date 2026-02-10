ec5298a4eeaafe693d01296ea391b2d5281bbb9d
James Lopez <jamestlopez.code@gmail.com>
2026-02-10 14:51:47 -0500

    fix(script): remove PCGL_DACO_ID

--- apps/api/src/config/dbConfig.ts
+++ apps/api/src/config/dbConfig.ts
@@ -25,7 +25,6 @@ const serverConfigSchema = z.object({
 	PG_PASSWORD: z.string(),
 	PG_HOST: z.string(),
 	PG_DATABASE: z.string(),
-	PCGL_DACO_ID: z.string(),
 });
 
 const parseResult = serverConfigSchema.safeParse(process.env);
