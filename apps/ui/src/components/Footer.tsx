01f97a5dc81db0fff4c34ed4a7596f55540bf945
James Lopez <jamestlopez.code@gmail.com>
2026-08-12 09:11:47 -0400

    fix: update env import

--- apps/ui/src/components/Footer.tsx
+++ apps/ui/src/components/Footer.tsx
@@ -43,7 +43,7 @@ const FooterComponent = () => {
 	const pcglLinks: LinkType[] = [
 		{
 			name: translate('links.contact'),
-			href: 'mailto:' + process.env.VITE_HELP_DESK_EMAIL,
+			href: 'mailto:' + import.meta.env.VITE_HELP_DESK_EMAIL,
 		},
 		{
 			name: translate('links.policies'),
